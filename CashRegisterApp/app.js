0844627a6d66bef195a29d0d3d00108d86ce9ea1
Hiren Jethva <54610864+hirenjethva@users.noreply.github.com>
2021-12-10 10:10:42 +0530

    Initial Commit

---
+++ CashRegisterApp/app.js
@@ -0,0 +1,45 @@
+const billAmount = document.querySelector("#bill-amount");
+const cashGiven = document.querySelector("#cash-given");
+const checkButton = document.querySelector("#button-check");
+const errorMessage = document.querySelector("#error-message");
+const noOfNotes = document.querySelectorAll(".no-of-notes");
+
+const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
+
+checkButton.addEventListener("click", function validate(){
+    hideError();
+    if(Number(billAmount.value) > 0)
+    {
+        if(Number(cashGiven.value) >= Number(billAmount.value))
+        {
+            const amountToBeReturned = Number(cashGiven.value) - Number(billAmount.value);
+            calculateChange(amountToBeReturned);
+        }
+        else
+        {
+            showError("Cash given should be atleast equal to Bill Amount.");
+        }
+    }
+    else
+    {
+        showError("Invalid Bill Amount!");
+    }
+});
+
+function calculateChange(amountToBeReturned){
+    for(var i=0; i<availableNotes.length; i++)
+    {
+        const numberOfNotes = Math.trunc(amountToBeReturned / availableNotes[i]);
+        amountToBeReturned = amountToBeReturned % availableNotes[i];
+        noOfNotes[i].innerText = numberOfNotes;
+    }
+}
+
+function hideError(){
+    errorMessage.style.display = "none";
+}
+
+function showError(msg){
+    errorMessage.style.display = "block";
+    errorMessage.innerText= msg;
+}
